854f1aaa47b9cb50308d59fc5ffb50b7b7ce25cd
pepepecas12000 <jjimenechuyin@gmail.com>
2024-07-04 13:52:27 -0600

    opciones

---
+++ opciones.js
@@ -0,0 +1,28 @@
+// const especie = document.getElementById("pet-select");
+// const raza= document.getElementById("raza-select");
+
+// const breeds = {
+//     perro: ['Bulldog', 'Labrador', 'Beagle'],
+//     gato: ['Siamés', 'Persa', 'Maine Coon'],
+//     hamster: ['Sirio', 'Roborovski', 'Chino'],
+//     perico: ['Opalino', 'Verde', 'Azul'],
+//     araña: ['Viuda Negra', 'Tarántula', 'Reclusa Marrón'],
+//     pez: ['Pez Dorado', 'Betta', 'Guppy']
+// };
+
+// especie.addEventListener('change',(event)=> {
+//     const especieselected = event.target.value;
+//     const opciones_raza = breeds[especieselected] || [] ;
+//     raza.innerHTML = '<option hidden>--Razas de animales--</option>';
+
+     
+//     opciones_raza.forEach(breed => {
+//         const opcion= document.createElement("option")
+//         opcion.value=breed;
+//         opcion.textContent=breed;
+//         raza.appendChild(opcion);
+
+
+//     });
+// })
+
